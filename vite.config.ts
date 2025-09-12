5f74a6b1ac6cfc2dc4e83ade925d2e985e24d52f
Jamie Sinclair <jamsinclairnz@gmail.com>
2025-09-12 17:03:55 +0900

    chore: fix build error on ci

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "node:url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
